2d2c765bf7e0679381eda660348448da074a0b92
Chennakesav1 <chennakesavarao89@gmail.com>
2026-04-06 10:12:55 +0530

    storng

--- server.js
+++ server.js
@@ -85,5 +85,5 @@ app.put('/api/workorders/:wo_no', async (req, res) => {
     }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
